b7096b13d86821cbce4ecd6c6d90140e39467fe4
Gustavo Rodriguez <gustavo.rodriguez@adevinta.com>
2020-03-17 11:42:20 +0100

    initial commit

---
+++ src/test/kotlin/com/esmarit/accesspointincomingendpoint/AccessPointIncomingEndpointApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.esmarit.accesspointincomingendpoint
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccessPointIncomingEndpointApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
