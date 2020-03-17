229db1b4f0f7fed25828d9ee10a1f46483baccf4
Gustavo Rodriguez <gustavo.rodriguez@adevinta.com>
2020-03-17 14:58:53 +0100

    fix: ci test

--- src/main/kotlin/com/esmarit/accesspointincomingendpoint/AccessPointIncomingEndpointApplication.kt
+++ src/main/kotlin/com/esmarit/accesspointincomingendpoint/AccessPointIncomingEndpointApplication.kt
@@ -7,6 +7,6 @@ import org.springframework.boot.runApplication
 class AccessPointIncomingEndpointApplication
 
 fun main(args: Array<String>) {
-    println("hello")
+    println("hello test")
     runApplication<AccessPointIncomingEndpointApplication>(*args)
 }
