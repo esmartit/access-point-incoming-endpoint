9fda687cd1a033632f594f908b68c802bde89d6b
Gustavo Rodriguez <gustavo.rodriguez@adevinta.com>
2020-08-24 09:33:55 +0200

    fix: adding log to device with position when sending

--- src/main/kotlin/com/esmarit/accesspointincomingendpoint/producer/EventProducer.kt
+++ src/main/kotlin/com/esmarit/accesspointincomingendpoint/producer/EventProducer.kt
@@ -1,5 +1,6 @@
 package com.esmarit.accesspointincomingendpoint.producer
 
+import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.messaging.Source
 import org.springframework.kafka.support.KafkaHeaders
@@ -7,7 +8,13 @@ import org.springframework.messaging.support.MessageBuilder
 
 @EnableBinding(Source::class)
 class EventProducer(private val source: Source) {
+
+    private val logger = LoggerFactory.getLogger(EventProducer::class.java)
+
     fun process(event: DeviceSeenEvent) {
+
+        logger.info("sending event: $event")
+
         event.run {
             source.output().send(
                 MessageBuilder
