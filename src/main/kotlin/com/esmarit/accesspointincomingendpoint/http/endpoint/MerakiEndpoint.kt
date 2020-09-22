3076097639ce5c3fd5ae83f5961c85c1efb93daf
Gustavo Rodriguez <gustavo.rodriguez@adevinta.com>
2020-09-22 14:29:23 +0200

    feat: upload files

--- src/main/kotlin/com/esmarit/accesspointincomingendpoint/http/endpoint/MerakiEndpoint.kt
+++ src/main/kotlin/com/esmarit/accesspointincomingendpoint/http/endpoint/MerakiEndpoint.kt
@@ -8,20 +8,33 @@ import com.esmarit.accesspointincomingendpoint.producer.DeviceLocation
 import com.esmarit.accesspointincomingendpoint.producer.DeviceSeen
 import com.esmarit.accesspointincomingendpoint.producer.DeviceSeenEvent
 import com.esmarit.accesspointincomingendpoint.producer.EventProducer
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.buffer.DataBufferUtils
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import reactor.core.scheduler.Schedulers
+import java.nio.charset.StandardCharsets
+
 
 @RestController
 @RequestMapping(value = ["/meraki"])
-class MerakiEndpoint(private val producer: EventProducer) {
+class MerakiEndpoint(
+    private val objectMapper: ObjectMapper,
+    private val producer: EventProducer
+) {
 
     private val logger = LoggerFactory.getLogger(MerakiEndpoint::class.java)
 
@@ -40,6 +53,30 @@ class MerakiEndpoint(private val producer: EventProducer) {
             .flatMapIterable { mapToDeviceSeenEvents(it) }
             .doOnNext { producer.process(it) }
             .reduce(ResponseEntity.accepted().build(), { t, _ -> t })
+    }
+
+    @PostMapping(
+        value = ["/upload-devices"],
+        consumes = [MediaType.MULTIPART_FORM_DATA_VALUE]
+    )
+    @ResponseStatus(value = HttpStatus.OK)
+    fun upload(@RequestPart("files") filePartFlux: Flux<FilePart>): Mono<ResponseEntity<String>> {
+        return filePartFlux
+            .flatMap { readLines(it) }
+            .map { objectMapper.readValue<MerakiPayload>(it.toString()) }
+            .map { it.data }
+            .flatMapIterable { mapToDeviceSeenEvents(it) }
+            .doOnNext { producer.process(it) }
+            .reduce(ResponseEntity.accepted().build(), { t, _ -> t })
+    }
+
+    private fun readLines(filePart: FilePart): Mono<StringBuilder> {
+        return filePart.content().map {
+            val bytes = ByteArray(it.readableByteCount())
+            it.read(bytes)
+            DataBufferUtils.release(it)
+            String(bytes, StandardCharsets.UTF_8)
+        }.reduce(StringBuilder()) { acc, curr -> acc.append(curr) }
     }
 
     private fun mapToDeviceSeenEvents(incomingData: Data): List<DeviceSeenEvent> {
